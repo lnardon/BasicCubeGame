a34150d9eba8a67c5499aaf0037783279a339c19
Lnardon <lucaslnardon@gmail.com>
2020-10-04 23:30:19 -0300

    Created basic game structure

---
+++ Assets/CubeMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+
+public class CubeMovement : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float sidewaysForce = 500f;
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rb.AddForce(0, 0, 2000 * Time.deltaTime);
+
+        if(Input.GetKey("d"))
+        {
+            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0);
+        }
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0);
+        }
+    }
+}
---
+++ Assets/PlayerCollision.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour
+{
+    public CubeMovement cm;
+
+    void OnCollisionEnter (Collision colInfo)
+    {
+        if(colInfo.collider.tag == "Obstacle")
+        {
+            cm.enabled = false;
+        }
+    }
+}
